c60992dfd067eab966ccb7a7a24d6adc9d19b34e
Ganesan <Ganesan@B-JAV-LAP-Ganesan>
2023-08-16 17:21:20 +0800

    post method added in controller

--- src/main/java/com/ispg/web/Controller.java
+++ src/main/java/com/ispg/web/Controller.java
@@ -1,6 +1,7 @@
 package com.ispg.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class Controller {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello World from ispgweb autom deploy now";
+	}
+	
+	@PostMapping
+	public String test() {
+		return "test";
 	}
 
 }
