c65f0a5f6e749c691f37a6e926057884c2cdc108
Ganesan <Ganesan@B-JAV-LAP-Ganesan>
2023-08-17 12:31:05 +0800

    added testpost api in HomeController

--- src/main/java/com/ispg/web/HomeController.java
+++ src/main/java/com/ispg/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.ispg.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,11 @@ public class HomeController {
 		
 		return "Test";
 	}
+	
+	@PostMapping("/testpost")
+	public String testpost() {
+		return "response from testpost api";
+	}
+	
 
 }
